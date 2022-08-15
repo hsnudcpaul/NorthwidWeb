4d4f020b8c8f8a10eb4fc2538ca196a1f2e3a270
俊廷 陳 <hsnudcpaul@hotmail.com>
2022-08-16 00:40:31 +0800

    加入專案檔案。

---
+++ NorthwidWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NorthwidWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
